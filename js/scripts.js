0f2f2faab9ac2ec7f732923957af359a904f9d0b
Kat Gifford and Sara Schultz <kat@>
2017-04-04 15:58:20 -0700

    add first prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
 //Back-End Logic:
-function BankAccount(userName, initialDeposit) {
+function BankAccount(userName, initialDeposit, depositAmount, withdrawalAmount) {
   this.userName = userName;
   this.initialDeposit = initialDeposit;
+  this.depositAmount = depositAmount;
+  this.withdrawalAmount = withdrawalAmount;
 }
 
+BankAccount.prototype.deposit = function () {
+  var balance = this.initialDeposit += this.depositAmount;
+}
 
+BankAccount.prototype.withdrawal = function () {
+ var balance = this.withdrawalDeposit += this.withdrawalAmount;
+}
 
 //Front-End Logic:
 $(document).ready(function() {
   $("#bank-form").submit(function(event) {
     event.preventDefault();
-
+    debugger;
     var inputtedName = $("#name").val();
     var inputtedInitialDeposit = parseFloat($("#initial-deposit").val());
     var inputtedDepositAmount = parseFloat($("#deposit-amount").val());
     var inputtedWithdrawalAmount = parseFloat($("#withdrawal-amount").val());
 
-    var newBankAccount = new BankAccount(inputtedName, inputtedInitialDeposit);
+    var newBankAccount = new BankAccount(inputtedName, inputtedInitialDeposit, inputtedDepositAmount, inputtedWithdrawalAmount);
 
-    var output = $("#current-balance").text(newBankAccount.initialDeposit);
+    var output = $("#current-balance").text(newBankAccount.deposit());
+    console.log(output);
   });
 });
